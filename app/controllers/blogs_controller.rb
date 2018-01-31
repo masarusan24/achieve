9a07d851a4349e8011ecec0364ebf79805fdd3e3
= <=>
2018-01-31 20:24:05 +0900

    Bootstrapシリーズ課題2 提出

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,11 +7,11 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
-      redirect_to root_path, notice: "ブログを作成しました！"
+      redirect_to blogs_path, notice: "ブログを作成しました！"
     else
       render 'new'
     end
@@ -23,33 +23,33 @@ class BlogsController < ApplicationController
 
   def destroy
     @blog.destroy
-    redirect_to root_path, notice: "ブログを削除しました"
+    redirect_to blogs_path, notice: "ブログを削除しました"
   end
-  
+
   def show
   end
 
   def edit
   end
-  
+
   def confirm
     @blog = Blog.new(blog_params)
     render 'new' if @blog.invalid?
   end
-  
+
   def update
     if @blog.update(blog_params)
-      redirect_to root_path, notice: "ブログを編集しました"
+      redirect_to blogs_path, notice: "ブログを編集しました"
     else
       render 'edit'
     end
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
