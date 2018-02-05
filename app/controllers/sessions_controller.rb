081c2a764010e5b70b84bd832ce952c6cb003dba
= <=>
2018-02-06 00:12:24 +0900

    add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,10 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:info] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
 end
