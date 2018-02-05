7de5dc40d889fe90918b42a96bb1b5e001ec9840
= <=>
2018-02-06 08:48:09 +0900

    edit flash message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,16 +2,17 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to new_contact_path, notice: "お問い合わせを受け付けました。"
+      flash[:info] = 'お問い合わせを受け付けました。'
+      redirect_to new_contact_path
     else
       render 'new'
     end
   end
-  
+
   private
   def contact_params
     params.require(:contact).permit(:name, :email, :content)
