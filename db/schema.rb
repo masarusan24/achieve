6dc59d96bff530e1034e5e32234223a03b62d1fc
= <=>
2018-02-08 01:00:21 +0900

    add association migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204213554) do
+ActiveRecord::Schema.define(version: 20180207155743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180204213554) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -36,6 +38,10 @@ ActiveRecord::Schema.define(version: 20180204213554) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "blog_id"
+    t.index ["blog_id"], name: "index_users_on_blog_id"
   end
 
+  add_foreign_key "blogs", "users"
+  add_foreign_key "users", "blogs"
 end
