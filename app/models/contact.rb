04cef062a62012703ffb62f151e64d7de97f3c13
= <=>
2018-02-11 17:12:19 +0900

    add validates to Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
   validates :name, presence: true
   validates :content, length: { in: 1..140 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
